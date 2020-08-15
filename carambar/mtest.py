f59afc245ef1b51eadf281321e3cf0aed7c70740
vjern <jocelyn.vernay@gmail.com>
2020-08-15 12:21:03 +0200

    Clean mtest.py

--- carambar/mtest.py
+++ carambar/mtest.py
@@ -45,46 +45,93 @@ def parse_action(action: str) -> (str, list):
 
 
 def prompt(msg: str, ans: str = '') -> str:
+
+    """
+    Prints a user prompt. Process the user's answer according to the specified parameters.
+    Returns the user answer (string character).
+    
+    -> Used without options (ans), this is meant to only notify the user of some information
+       before continuing on with the rest of the program. (Press Enter to continue)
+    -> ans is meant to be a list (or string) of one-letter possible answers; the user must
+       provide an answer contained in the list, or they will be prompted again until they do so.
+    -> You can set a default answer by having it in uppercase in the `ans` array:
+       >>> prompt('Proceed ?', 'Yn')
+       The first uppercase answer found in the answer array will be designated as the default answer.
+       A default answer allows the user to directly press enter instead of having to input anything.
+    """
+
     user_msg = ' '.join((msg, '(%s)' % ''.join(ans) if bool(ans) else '(Press Enter to continue)')) + ' '
+    
     user_ans = input(user_msg).strip()
+
     if not ans:
         return
+        
     default_answer = get_default_answer(ans)
+
     if default_answer is not None and not user_ans:
         return default_answer
+
     while len(user_ans) != 1 or user_ans not in ans:
         user_ans = input(user_msg)
         if default_answer is not None and not user_ans:
             return default_answer
+
     return user_ans
 
-def ask(msg: str):
-    assert prompt(msg, 'Yn') == 'Y'
+
+def yesno(msg: str):
+    """Ask a yes-no question, and returns whether the answer was 'yes'."""
+    return prompt(msg, 'Yn') == 'Y'
 
 
 @contextmanager
 def Test(action: str, question: str = ''):
+
+    """
+    Start a human-driven test. First prompt the user with the description of what the test is about,
+    run the test,
+    then have the user assess whether the test has succeeded through a second prompt.
+    Raise an AssertionError otherwise.
+
+    :param action:
+        The 'action' that the test will perform and that the user should evaluate. 
+        This should be a string describing the action, eg "print yellow text then download data to a given file."
+    :param question:
+        If only 'action' is provided, both pre-test and post-test prompts will show the same text.
+        This parameter allows you to provide a different text for the post-test prompt.
+    """
+    
     color = '\033[38;2;250;180;40m'
     action, keywords = parse_action(action)
     desc = color + '{ This program will now %s. }\033[m' % action
+    
     if not question:
         question = 'Did the program %s ?' % action
     question = color + '{ %s }\033[m' % question
+    
     name = ' '.join(keywords)
+    
     if not name:
         name = action[:30]
         if len(action) > 30:
             name = name[:29] + '/'
+    
     print('\033[95m{ Oracle Test }\033[m')
     prompt(desc)
+    
+    #
     yield
+    #
+
     try:
-        ask(question)
+        assert yesno(question)
+    
     except AssertionError:
         print('\033[91m%s\033[m' % '[ Test { %s } failed. ]' % (name))
         raise
-    print('\033[92m%s\033[m' % '[ Test { %s } was a success. ]' % (name))
 
+    print('\033[92m%s\033[m' % '[ Test { %s } was a success. ]' % (name))
 
 
 def test_Test():
@@ -93,17 +140,29 @@ def test_Test():
 
 
 def run_tests(env):
-    # print(env['__file__'], end=' ')
+
+    """
+    Run test functions in a given environment dict not unlike pytest.
+    Keep track of successes and failures, including error messages.
+    Can be used to run test functions found in a module this way:
+    >>> run_tests(globals())
+    """
+
     results = []
+
     for key, value in env.items():
+
         if not key.startswith('test_'):
             continue
+
         try:
             value()
             results += [(key, '.', None)]
         except Exception:
             results += [(key, 'F', traceback.format_exc())]
+
     print(env['__file__'], '{', len([r for r in results if r[1] == '.']), '/', len(results), '}')
+
     for name, result, message in results:
         print('- {:30} {}'.format(name, result))
         if message is not None:
