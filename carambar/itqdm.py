3964fcbaa4d15dfd9a30c6ea76bebe6d347c619e
zebralt <jocelyn.vernay@ipsos.com>
2020-08-25 13:03:32 +0200

    Move tqdm integration away from deprecated _environ_cols_wrapper

--- carambar/itqdm.py
+++ carambar/itqdm.py
@@ -3,8 +3,9 @@ from . import termset
 import os
 
 
-def _get_cols(fp: int) -> int: 
-    return termset.get_terminal_size().columns
+def _get_screen_shape(fp: int) -> int: 
+    size = termset.get_terminal_size()
+    return size.columns, size.lines
 
 
 def tqdm(*args, **kwargs):
@@ -12,7 +13,7 @@ def tqdm(*args, **kwargs):
     import tqdm  # type: ignore
 
     # To enable tqdm dynamic resizing
-    tqdm._tqdm._environ_cols_wrapper = lambda: _get_cols
+    tqdm.std._screen_shape_wrapper = lambda: _get_screen_shape
 
     if 'file' in kwargs:
         return tqdm.tqdm(*args, **kwargs)
