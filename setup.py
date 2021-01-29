ae9525923a2f92e4396d0e0d831174ea378888e5
Jocelyn Vernay <59055698+vjern@users.noreply.github.com>
2021-01-29 16:27:22 +0100

    Add proper setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='carambar',
+    author='zebralt',
+    version='1.0.0',
+    packages=['carambar'],
+    python_requires='>=3.6',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    url='https://github.com/zebralt/carambar'
+)
